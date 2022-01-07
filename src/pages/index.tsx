7f4a74e3cb982554bfdb7d15a69e760e3ade1779
Matt Phillips <matt@mattphillips.io>
2022-01-07 16:52:34 +0000

    Get posts on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
 import React from "react";
 
-export default function Home() {
-  return <h1 className="text-3xl ">Hello world!</h1>;
+import { GetStaticProps } from "next";
+import { getAllPosts } from "src/lib/api";
+
+type Home = {
+  posts: Array<{
+    date: string;
+    title: string;
+  }>;
+};
+
+export default function Home({ posts }: Home) {
+  return (
+    <ul>
+      {posts.map((p) => (
+        <li>{JSON.stringify(p)})</li>
+      ))}
+    </ul>
+  );
 }
+
+export const getStaticProps: GetStaticProps<Home> = async () => {
+  const posts = getAllPosts();
+
+  return {
+    props: {
+      posts: posts.map((p) => p.data as any),
+    },
+  };
+};
