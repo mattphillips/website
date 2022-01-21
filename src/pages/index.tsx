44932499cc8704cc6b60af2772886e66fe4edab6
Matt Phillips <matt@mattphillips.io>
2022-01-21 14:32:13 +0000

    Add simple footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,6 +87,30 @@ export default function Home(props: Home) {
           </li>
         ))}
       </ul>
+      <footer className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
+            <a href="https://twitter.com/mattphillipsio" target="_blank" title="Twitter" rel="noopener">
+              <div className="border-2 border-solid border-black rounded-full p-3 mr-6">
+                <Twitter />
+              </div>
+            </a>
+            <a href="https://github.com/mattphillips" target="_blank" title="Github" rel="noopener">
+              <div className="border-2 border-solid border-black rounded-full p-3 mr-6">
+                <Github />
+              </div>
+            </a>
+            <a href="https://linkedin.com/in/mattphillipsio" target="_blank" title="Linked" rel="noopener">
+              <div className="border-2 border-solid border-black rounded-full p-3 mr-6">
+                <LinkedIn />
+              </div>
+            </a>
+          </div>
+          <div className="mt-8 md:mt-0 md:order-1">
+            <p className="text-center text-base text-gray-400">&copy; 2022 Matt Phillips. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
