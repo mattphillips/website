6395035dd3ecbfcca093bb6fc1a665468758759e
Matt Phillips <matt@mattphillips.io>
2022-01-21 19:57:51 +0000

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ class MyDocument extends Document<{ theme: string }> {
     return (
       <Html lang="en">
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link
             href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Open+Sans:wght@300;400;600;700&display=swap"
             rel="stylesheet"
